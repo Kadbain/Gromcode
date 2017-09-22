5704e9ead209eedbf06c413528261570f1e6fef5
Kadbain <ps1x0209@gmail.com>
2017-09-22 17:38:42 +0300

    C_15

---
+++ src/lesson9/dz/UserRepository.java
@@ -0,0 +1,39 @@
+package lesson9.dz;
+
+/**
+ * Created by Ps1X on 22.09.2017.
+ */
+public class UserRepository{
+    private User[] users;
+
+    public UserRepository(User user) {
+        this.users = new User[]{new User(user.getId(), user.getName(), user.getSessionId())};
+    }
+
+    public User[] getUsers() {
+        return users;
+    }
+
+    public String [] getUserNames(User user) {
+        String [] names = new String[]{};
+        for (int i = 0; i < users.length; i++) {
+            names[i] = users[i].getName();
+        }
+        return names;
+    }
+    public long [] getUserIds() {
+        long [] ids = new long[]{};
+        for (int i = 0; i < users.length; i++) {
+            ids[i] = users[i].getId();
+        }
+        return ids;
+    }
+    public String getUserNameById(long id) {
+        for (int i = 0; i < users.length; i++) {
+            if (id == users[i].getId()) {
+                return users[i].getName();
+            }
+        }
+        return "don't finded user";
+    }
+}
