c5565f6a0babb89faa19c4a0d16acce381e664f8
Kadbain <ps1x0209@gmail.com>
2017-09-22 16:41:25 +0300

    C_14

---
+++ src/lesson9/dz/User.java
@@ -0,0 +1,22 @@
+package lesson9.dz;
+
+/**
+ * Created by Ps1X on 22.09.2017.
+ */
+public class User {
+    private long id;
+    private String name;
+    private String sessionId;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+}
