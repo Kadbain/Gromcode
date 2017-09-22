39e184c9db75d0a46ffe1b5a739b76207f7dfb81
Kadbain <ps1x0209@gmail.com>
2017-09-22 20:29:30 +0300

    C_16

--- src/lesson9/dz/UserRepository.java
+++ src/lesson9/dz/UserRepository.java
@@ -7,31 +7,32 @@ public class UserRepository{
     private User[] users;
 //this.users = new User[]{new User(user.getId(), user.getName(), user.getSessionId())};
 
-    public UserRepository(User[] users, User user) {
-        users = new User[]{new User(user.getId(), user.getName(), user.getSessionId())};
+
+    public UserRepository(User[] users) {
         this.users = users;
     }
 
     public User[] getUsers() {
+
         return users;
     }
 
     public String [] getUserNames() {
-        String [] names = new String[]{};
+        String [] names = new String[getUsers().length];
         for (int i = 0; i < getUsers().length; i++) {
             names[i] = getUsers()[i].getName();
         }
         return names;
     }
     public long [] getUserIds() {
-        long [] ids = new long[]{};
+        long [] ids = new long[getUsers().length];
         for (int i = 0; i < getUsers().length; i++) {
             ids[i] = getUsers()[i].getId();
         }
         return ids;
     }
     public String getUserNameById(long id) {
-        for (int i = 0; i < users.length; i++) {
+        for (int i = 0; i < getUsers().length; i++) {
             if (id == getUsers()[i].getId()) {
                 return getUsers()[i].getName();
             }
