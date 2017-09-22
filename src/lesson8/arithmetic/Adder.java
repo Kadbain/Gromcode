2808e14c56178b282b08451f3d4efb67e3aa5af1
Kadbain <ps1x0209@gmail.com>
2017-09-22 08:45:21 +0300

    C_07

--- src/lesson8/arithmetic/Adder.java
+++ src/lesson8/arithmetic/Adder.java
@@ -1,8 +1,5 @@
 package lesson8.arithmetic;
 
-/**
- * Created by Ps1X on 22.09.2017.
- */
 public class Adder extends Arithmetic{
     public int add(int a, int b) {
         int c = a + b;
--- src/lesson8/arithmetic/Demo.java
+++ src/lesson8/arithmetic/Demo.java
@@ -1,12 +1,9 @@
 package lesson8.arithmetic;
 
-/**
- * Created by Ps1X on 22.09.2017.
- */
 public class Demo {
     public static void main(String[] args) {
         Adder adder = new Adder();
         adder.check(new int[]{1, 5, 7, 12, 21});
-        adder.add(12, -2);
+        adder.add(12, 2);
     }
 }
