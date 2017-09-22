39cf8ee2943fd82f986bbaa087e6ca2aca093d6b
Kadbain <ps1x0209@gmail.com>
2017-09-22 09:43:13 +0300

    C_12

--- src/lesson8/course/Demo.java
+++ src/lesson8/course/Demo.java
@@ -7,11 +7,9 @@ public class Demo {
     public static void main(String[] args) {
 
     }
-    public Course createHighestParent() {
-        Course course = new Course(new Date(), "Java Core", 8, "Andru",
-                new Student[]{new Student("jenya", "Utkin", 2, new Course[]{}),
-                              new Student("Vanya", "Putin", 2, new Course[]{})});
-        return course;
+    public Student createHighestParent() {
+        Student student = new Student("jenya", "Utkin", 2, new Course[]{});
+        return student;
     }
     public SpecialStudent createLowestChild() {
         SpecialStudent specialStudent= new SpecialStudent("Jenya", "Utkin", 2, new Course[]{}, 909,"ps1x0209@gmail.com");
