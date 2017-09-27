d1856e450b35198025d6bae5e847d86f4abb050b
Kadbain <ps1x0209@gmail.com>
2017-09-27 11:47:57 +0300

    C_20

--- src/lesson9/dz/UserRepository.java
+++ src/lesson9/dz/UserRepository.java
@@ -116,16 +116,32 @@ public class UserRepository {
         }
         return null;
     }
+    private int indexOfUser(long id){
+        int index=0;
+        for(int i=0; i<users.length;i++){
+            if (users[i] == null || users[i].getId() !=id)
+                index++;
+            else break;
+        }
+        return index;
+    }
+
     public void delete(long id) {
         if (findById(id) != null) {
-            for (int i = 0; i < getUsers().length; i++) {
-                if (getUsers()[i].getId() == id) {
-                    break;
-                }
-                for (int j = i; j < getUsers().length - 1; j++) {
-                    getUsers()[j] = getUsers()[j + 1];
-                }
-            }
+            users[indexOfUser(id)] = null;
         }
     }
+
+//    public void delete(long id) {
+//        if (findById(id) != null) {
+//            for (int i = 0; i < getUsers().length; i++) {
+//                if (getUsers()[i].getId() == id) {
+//                    break;
+//                }
+//                for (int j = i; j < getUsers().length - 1; j++) {
+//                    getUsers()[j] = getUsers()[j + 1];
+//                }
+//            }
+//        }
+//    }
 }
